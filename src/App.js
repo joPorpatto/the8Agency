b32a775fa67de216a95462839f750b85d5382884
joPorpatto <joel.porpatto@gmail.com>
2022-04-22 17:01:24 -0300

    section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './components/header/Header';
+import { LeftSection } from './components/leftSection/LeftSection';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <LeftSection/>
     </div>
   );
 }
---
+++ src/components/leftSection/LeftSection.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './leftSection.css';
+import IMG from '../../assets/img-aurnheimer.png';
+
+
+const especiaList = [
+  {
+  id:1,
+  name:'José Perez',
+  position:'Presidente',
+  icon:IMG,  
+  },
+  {
+  id:2,
+  name:'José Perez',
+  position:'Customer Service Senior Manager',
+  icon:IMG,  
+  },
+  {
+  id:3,
+  name:'José Perez',
+  position:'Senior Customer Success Consultant',
+  icon:IMG,  
+  },
+  {
+  id:4,  
+  name:'José Perez',
+  position:'Gerente de Consultoría de Soluciones',
+  icon:IMG,  
+  },
+  
+]
+
+export const LeftSection = () => {
+  return (
+    <section>
+      <div className='section___leftSection'>
+        <p>Te invitan este webinar, donde trataremos la temática de cómo humanizar la experiencia del cliente de Banca y Seguros en el nuevo entorno digital. <br/><br/>Además podremos conocer las estrategias que aplicó LOREM para generar una experiencia memorable para sus clientes, mientras se convertía en el gran Unicornio de LATAM. <br/><br/> Escucha de primera mano la voz de nuestros especialistas: </p>
+        <div className='section__cards'>
+          {
+           especiaList.map(especialist => {
+            return(
+              <div className='section__card' key={especialist.id}>
+                <img  src={IMG} alt='circle'  />
+                <div className='card__info'>
+                  <p className='info__name'>{especialist.name}</p>
+                  <p className='info__position'>{especialist.position}</p>                 
+                </div>                
+              </div>
+            )
+           })
+          }          
+        </div>    
+      <p>Participa e inspírate para innovar y mejorar la interacción entre clientes y marcas con historias de éxito de empresas del ámbito financiero en América Latina.<br/><br/> ¡Te esperamos!
+      </p>
+      
+      </div>
+
+    
+    </section>
+  )
+}
